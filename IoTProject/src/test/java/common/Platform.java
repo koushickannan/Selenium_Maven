7fdec1c1829fc850d21651eb6cfce7d8933c245b
Koushic Kannan <koushic-kannan.raju@hpe.com>
2017-01-19 11:48:47 +0530

    New files

--- IoTProject/src/test/java/common/Platform.java
+++ IoTProject/src/test/java/common/Platform.java
@@ -1,40 +1,147 @@
 package common;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.io.File;
+
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Platform {
 
-	public WebDriver driver;
+//	public WebDriver driver;
 
-	@Test
+	private String computerName;
+    private String OS;
+    private String OSFullNameAndVersion;
+    private String browser;
+    private String browserFullNameAndVersion;
+    private String platformOs;
+    PropertyHelper propsHelper = new PropertyHelper();
 
-	// Here this parameters we will take from testng.xml
-	@Parameters("Browser")
-	public void test1(String browser) {
+    public void setPlatformOs(String platformOs) {
+        this.platformOs = platformOs;
+    }
 
-		if (browser.equalsIgnoreCase("FF")) {
+    public String getPlatformOs() {
+        return platformOs;
+    }
 
-			driver = new FirefoxDriver();
+    public Platform() throws Exception {
+    }
 
-		} else if (browser.equalsIgnoreCase("IE")) {
+    public String getComputerName() {
+        return computerName;
+    }
 
-			System.setProperty("webdriver.ie.driver", "./IEDriverServer/IEDriverServer.exe");
+    public void setComputerName(String computerName) {
+        this.computerName = computerName;
+    }
 
-			driver = new InternetExplorerDriver();
+    public String getOS() {
+        return OS;
+    }
 
-		} else if (browser.equalsIgnoreCase("CH")) {
+    public void setOS(String oS) {
+        OS = oS;
+    }
 
-			System.setProperty("webdriver.ie.driver", "./CHDriver/chromedriver.exe");
+    public String getOSFullNameAndVersion() {
+        return OSFullNameAndVersion;
+    }
 
-			driver = new ChromeDriver();
+    public void setOSFullNameAndVersion(String oSFullNameAndVersion) {
+        OSFullNameAndVersion = oSFullNameAndVersion;
+    }
 
-		}
-	}
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getBrowserFullNameAndVersion() {
+        return browserFullNameAndVersion;
+    }
+
+    public void setBrowserFullNameAndVersion(String browserFullNameAndVersion) {
+        this.browserFullNameAndVersion = browserFullNameAndVersion;
+    }
+
+    public String getOsOfTestPlatform() {
+        return platformOs;
+    }
+
+    public void setOsOfTestPlatform() {
+
+        this.platformOs = getCurrentPlatformOs();
+    }
+
+    private String getCurrentPlatformOs() {
+
+        String testOS = "N/A";
+
+        if (SystemUtils.IS_OS_MAC_OSX) {
+            testOS = "MAC";
+        }
+        if (SystemUtils.IS_OS_WINDOWS) {
+            testOS = "WIN";
+        }
+
+        return testOS;
+    }
+
+    public void IE(DesiredCapabilities capabilities) throws Exception {
+        capabilities.setBrowserName("internet explorer");
+        capabilities.setCapability("ignoreZoomSetting", true);
+        capabilities.setCapability("nativeEvents", false);
+        capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        File file = new File("IEDriverServer/IEDriverServer.exe");
+        System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+    }
+
+    public void BS(DesiredCapabilities capabilities) throws Exception {
+
+//        capabilities.setCapability("browser", propsHelper.readInitProperties("BROWSERSTACK.browser"));
+//        capabilities.setCapability("browser_version", propsHelper.readInitProperties("BROWSERSTACK.browser_version"));
+//        capabilities.setCapability("os", propsHelper.readInitProperties("BROWSERSTACK.os"));
+//        capabilities.setCapability("os_version", propsHelper.readInitProperties("BROWSERSTACK.os_version"));
+//        capabilities.setCapability("browserstack.debug", propsHelper.readInitProperties("BROWSERSTACK.debug"));
+
+    }
+
+    public FirefoxProfile FF(DesiredCapabilities capabilities) throws Exception {
+        FirefoxProfile profile = new FirefoxProfile();
+        capabilities.setBrowserName("firefox");
+        System.out.println("Staring FF Driver");
+        return profile;
+    }
+
+    public void CH(DesiredCapabilities capabilities) throws Exception {
+
+        String filePath = "";
+        capabilities.setBrowserName("chrome");
+
+        if (this.platformOs.equalsIgnoreCase("mac")) {
+
+            filePath = "CHDriverServer/chromedriver";
+        }
+
+        if (this.platformOs.equalsIgnoreCase("win")) {
+            filePath = "CHDriverServer/chromedriver.exe";
+        }
+
+        File file = new File(filePath);
+
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+    }
 
 }
--- IoTProject/src/test/java/common/PropertyHelper.java
+++ IoTProject/src/test/java/common/PropertyHelper.java
@@ -1,5 +1,52 @@
 package common;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropertyHelper {
+	
+	public Properties readProperties(String name) throws IOException {
+        Properties properties = new Properties();
+        ClassLoader loader = PropertyHelper.class.getClassLoader();
+        InputStream inStream = loader.getResourceAsStream(name);
+        properties.load(inStream);
+        return properties;
+    }
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public String getInitProperties(String key) {
+
+        Properties props = null;
+        String value = "";
+
+       String environmentIdentifier = this.getENV();
+
+        try {
+//            props = readProperties(INIT_PROPERTY_FILE);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        if (key.equals("BROWSER.type")) {
+            value = props.getProperty(key);
+        } else {
+            value = props.getProperty(environmentIdentifier + "." + key);
+            try {
+//                props = readProperties(environmentIdentifier + "_" + INIT_PROPERTY_FILE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            value = props.getProperty(key);
+        }
+
+        return value;
+    }
+
 
 }
